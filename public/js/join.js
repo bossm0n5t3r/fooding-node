7cab1cb09ab1472946c54fe9fed34e07ed1fe1fb
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-08-27 22:30:04 +0900

    Update join.js Add ajax part

--- public/js/join.js
+++ public/js/join.js
@@ -1,7 +1,38 @@
 $(document).ready(function(){
+  $("#register-inputUserame").keyup(function() {
+    var inputUserame = $("#register-inputUserame").val();
+    if (inputUserame.length == 0) {
+      $("#id-check").css("color", "red");
+      $("#id-check").html('아이디를 입력해주세요.');
+    } else if (inputUserame.length < 4) {
+      $("#id-check").css("color", "red");
+      $("#id-check").html('아이디는 4자 이상, 15자 이하입니다.');
+    } else if (inputUserame.length > 15) {
+      $("#id-check").css("color", "red");
+      $("#id-check").html('아이디는 4자 이상, 15자 이하입니다.');
+    } else {
+      $("#id-check").css("color", "");
+      $("#id-check").html('');
+    }
+  });
+  
+  $("#id-check-btn").click(function() {
+    var inputUserame = $("#register-inputUserame").val();
+    $.post("/ajax/id-check", { user_name: inputUserame }, function(msg) {
+      if (msg.result == 1) {
+        $("#id-check").css("color", "blue");
+        $("#id-check").html(inputUserame + '은(는) 사용할 수 있는 아이디 입니다.');
+      } else {
+        $("#id-check").css("color", "red");
+        $("#id-check").html(inputUserame + '은(는) 사용할 수 없는 아이디 입니다.');
+      }
+    }, 'json');
+  });
+
   $("#registerBtn").click(function() {
     join();
   });
+
 });
 
 function join() {
