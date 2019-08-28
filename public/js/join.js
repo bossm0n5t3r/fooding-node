f6ef9f4f33135d70b33740c8eed5780547d94c1b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-08-28 15:21:46 +0900

    Update join.js Remove id-check-btn

--- public/js/join.js
+++ public/js/join.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var notIdCheck = true;
-  var tmpSaveUserId = '';
+  var notEmailCheck = true;
 
   $("#register-inputUserame").keyup(function() {
     var inputUserame = $("#register-inputUserame").val();
@@ -14,29 +14,12 @@ $(document).ready(function() {
       $("#id-check").css("color", "red");
       $("#id-check").html('아이디는 4자 이상, 15자 이하입니다.');
     } else {
-      $("#id-check").css("color", "");
-      $("#id-check").html('');
-    }
+      idCheck();
+    };
   });
-  
-  $("#id-check-btn").click(function() {
-    var inputUserame = $("#register-inputUserame").val();
-    if (inputUserame.length == 0) {
-      $("#id-check").css("color", "red");
-      $("#id-check").html('아이디를 입력해주세요.');
-    } else {
-      $.post("/ajax/id-check", { user_name: inputUserame }, function(msg) {
-        if (msg.result == 1) {
-          $("#id-check").css("color", "blue");
-          $("#id-check").html(inputUserame + '은(는) 사용할 수 있는 아이디 입니다.');
-          notIdCheck = false;
-          tmpSaveUserId = inputUserame;
-        } else {
-          $("#id-check").css("color", "red");
-          $("#id-check").html(inputUserame + '은(는) 사용할 수 없는 아이디 입니다.');
-        }
-      }, 'json');
-    }
+
+  $("#register-inputEmail").keyup(function() {
+    emailCheck();
   });
 
   $("#registerBtn").click(function() {
@@ -47,10 +30,9 @@ $(document).ready(function() {
     if (inputUserame.length == '') {
       alert('아이디를 입력해주세요.');
     } else if (notIdCheck) {
-      alert('아이디 중복체크를 누르세요.');
-    } else if (tmpSaveUserId !== inputUserame) {
-      notIdCheck = true;
-      alert('아이디 중복체크를 누르세요.');
+      alert('아이디를 다시 확인해주세요.');
+    } else if (notEmailCheck) {
+      alert('이메일을 다시 확인해주세요.');
     } else if (inputEmail.length == '') {
       alert('이메일을 입력해주세요.');
     } else if (inputPassword.length == '') {
@@ -61,6 +43,46 @@ $(document).ready(function() {
       alert('입력된 비밀번호가 서로 다릅니다.');
     } else {
       $("#registerForm").submit();
-    }
+    };
   });
+
+  function idCheck() {
+    var inputUserame = $("#register-inputUserame").val();
+    if (inputUserame.length == 0) {
+      $("#id-check").css("color", "red");
+      $("#id-check").html('아이디를 입력해주세요.');
+    } else {
+      $.post("/ajax/id-check", { user_name: inputUserame }, function(msg) {
+        if (msg.result == 1) {
+          $("#id-check").css("color", "");
+          $("#id-check").html('');
+          notIdCheck = false;
+        } else {
+          $("#id-check").css("color", "red");
+          $("#id-check").html(inputUserame + '은(는) 사용할 수 없는 아이디 입니다.');
+          notIdCheck = true;
+        }
+      }, 'json');
+    };
+  };
+  
+  function emailCheck() {
+    var inputEmail = $("#register-inputEmail").val();
+    if (inputEmail.length == 0) {
+      $("#email-check").css("color", "red");
+      $("#email-check").html('이메일을 입력해주세요.');
+    } else {
+      $.post("/ajax/email-check", { user_email: inputEmail }, function(msg) {
+        if (msg.result == 1) {
+          $("#email-check").css("color", "");
+          $("#email-check").html('');
+          notEmailCheck = false;
+        } else {
+          $("#email-check").css("color", "red");
+          $("#email-check").html('이미 가입된 이메일 입니다.');
+          notEmailCheck = true;
+        }
+      }, 'json');
+    };
+  };
 });
