77961e3bf6bbdf970f7e937d25d456dc62309fda
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-09-02 10:14:45 +0900

    Add profile.js

---
+++ public/js/profile.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $("#profile-back-btn").click(function() {
+    let check = confirm("돌아가면 모든 내용은 지워집니다. 돌아가시겠습니까?");
+    if (check) {
+      window.location.href = "http://localhost:8001/main";
+    }
+  });
+
+  $("#profile-edit-btn").click(function() {
+    var inputPassword = $("#profile-inputPassword")
+      .val()
+      .trim();
+    var inputConfirmPassword = $("#profile-inputConfirmPassword")
+      .val()
+      .trim();
+    if (inputPassword !== inputConfirmPassword) {
+      alert("비밀번호가 서로 일치하지 않습니다.");
+    } else {
+      $("#profile").submit();
+    }
+  });
+});
