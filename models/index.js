45d3f68e72ed221cf5b07390dba8e4ceea78dc3d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-09-08 23:09:20 +0900

    Apply Prettier

--- models/index.js
+++ models/index.js
@@ -1,19 +1,22 @@
-const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env];
 const db = {};
 
 const sequelize = new Sequelize(
-  config.database, config.username, config.password, config,
+  config.database,
+  config.username,
+  config.password,
+  config
 );
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-db.User = require('./user')(sequelize, Sequelize);
-db.Store = require('./store')(sequelize, Sequelize);
-db.StoreCategory = require('./store_category')(sequelize, Sequelize);
-db.StoreReview = require('./store_review')(sequelize, Sequelize);
+db.User = require("./user")(sequelize, Sequelize);
+db.Store = require("./store")(sequelize, Sequelize);
+db.StoreCategory = require("./store_category")(sequelize, Sequelize);
+db.StoreReview = require("./store_review")(sequelize, Sequelize);
 
 db.User.hasOne(db.Store);
 db.Store.belongsTo(db.User);
