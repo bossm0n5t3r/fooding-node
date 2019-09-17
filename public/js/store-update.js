04365811b220688992ee50590cf186e670277abb
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-09-17 21:18:56 +0900

    Update store-update part

--- public/js/store-update.js
+++ public/js/store-update.js
@@ -67,4 +67,54 @@ $(document).ready(function() {
       $("#store-register").submit();
     }
   });
+
+  $("#store-modify-btn").click(function() {
+    var storeName = $("#store-name")
+      .val()
+      .trim();
+    var storeAddress = $("#store-address")
+      .val()
+      .trim();
+    var storeStartTime = $("#store-start-time")
+      .val()
+      .trim();
+    var storeEndTime = $("#store-end-time")
+      .val()
+      .trim();
+    var storeMenu1 = $("#store-menu-1")
+      .val()
+      .trim();
+    var storePrice1 = $("#store-price-1")
+      .val()
+      .trim();
+    var storeMenu2 = $("#store-menu-2")
+      .val()
+      .trim();
+    var storePrice2 = $("#store-price-2")
+      .val()
+      .trim();
+    if (storeName.length == 0) {
+      alert("가게 이름을 입력해주세요!");
+    } else if (storeAddress.length == 0) {
+      alert("가게 주소를 입력해주세요!");
+    } else if (storeStartTime.length == 0) {
+      alert("영업시작시간을 입력해주세요!");
+    } else if (storeEndTime.length == 0) {
+      alert("영업종료시간을 입력해주세요!");
+    } else if (storeMenu1.length == 0) {
+      alert("메뉴이름을 입력해주세요!");
+    } else if (storePrice1.length == 0) {
+      alert("메뉴가격을 입력해주세요!");
+    } else if (storeMenu2.length > 0) {
+      if (storePrice2.length == 0) {
+        alert("메뉴가격을 입력해주세요!");
+      }
+    } else if (storePrice2.length > 0) {
+      if (storeMenu2.length == 0) {
+        alert("메뉴이름을 입력해주세요!");
+      }
+    } else {
+      $("#store-modify").submit();
+    }
+  });
 });
